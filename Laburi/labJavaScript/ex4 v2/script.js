e35f8be018ff5adc83cfb2f9f01b1c9b0c4bf5de
Gabriela T <54414302+EcoGamer18@users.noreply.github.com>
2022-04-19 08:16:34 +0300

    Cerinta 4 V2 Done

---
+++ Laburi/labJavaScript/ex4 v2/script.js
@@ -0,0 +1,47 @@
+const sortColumn = (columnHeader) =>{
+    var sortCriteria = columnHeader.innerHTML;
+    var table = columnHeader.parentNode.parentNode.parentNode;
+    let toSort = new Array(table.rows[0].length);
+    for (var i = 0, row; row = table.rows[i]; i++) {
+        if (row.cells[0].innerHTML == sortCriteria) {
+            toSort = [].slice.call(row.cells);
+            toSort = toSort.slice(1, toSort.length);
+        }
+    }
+    var indexArray = [...Array(toSort.length).keys()];
+
+    let sorted = false;
+    while (!sorted) {
+        sorted = true;
+        for (let i = 0; i < toSort.length; i++) {
+            let el1 = toSort[i].innerHTML;
+            for (let j = i + 1; j < toSort.length; j++) {
+                let el2 = toSort[j].innerHTML;
+                if (!isNaN(el1)) {
+                    if (parseInt(el1) > parseInt(el2)) {
+                        [toSort[i], toSort[j]] = [toSort[j], toSort[i]];
+                        [indexArray[i], indexArray[j]] = [indexArray[j], indexArray[i]];
+                        sorted = false;
+                    }
+                }
+                else {
+                    if (el1 > el2) {
+                        [toSort[i], toSort[j]] = [toSort[j], toSort[i]];
+                        [indexArray[i], indexArray[j]] = [indexArray[j], indexArray[i]];
+                        sorted = false;
+                    }
+                }
+            }
+        }
+    }
+
+    for (var i = 0, row; row = table.rows[i]; i++) {
+        let sortRow = new Array(row.cells.length);
+        for (var j = 1; j < row.cells.length; j++) {
+            sortRow[j] = row.cells[indexArray[j - 1] + 1].innerHTML;
+        }
+        for (var j = 1; j < sortRow.length; j++) {
+            row.cells[j].innerHTML = sortRow[j];
+        }
+    }
+}
